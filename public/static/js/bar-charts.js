c5d13783fbc9d455ee0ee6d2f56c0b08a7c53a60
sumuh <muhli.susanna@gmail.com>
2023-05-12 23:11:26 +0300

    Add sorting to bar chart
    User can now sort bar chart ascending or descending by clicking on a
    button.

--- public/static/js/bar-charts.js
+++ public/static/js/bar-charts.js
@@ -4,6 +4,9 @@ const barChartMargin = { top: 20, right: 20, bottom: 20, left: 20 };
 const barChartWidth = 700 - barChartMargin.left - barChartMargin.right;
 const barChartHeight = 500 - barChartMargin.top - barChartMargin.bottom;
 
+const sortUpHtml = "<svg id='sort-up-icon' xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' class='bi bi-sort-up' viewBox='0 0 16 16'><path d='M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z'/></svg>"
+const sortDownHtml = "<svg id='sort-down-icon' xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' class='bi bi-sort-down' viewBox='0 0 16 16'><path d='M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z'/></svg>"
+
 const maxBarLabelWidth = 95;
 
 const barChartXScale = d3.scaleLinear()
@@ -20,9 +23,10 @@ const barChartTooltip = initBarChartToolTip();
 
 initBarChartXAxis();
 initBarChartYAxis();
+handleSort();
 
 export function initBarCharts() {
-    return d3.select('#charts-container')
+    return d3.select('#bar-chart-container')
         .append('svg')
         .attr("width", barChartWidth + barChartMargin.left + barChartMargin.right)
         .attr("height", barChartHeight + barChartMargin.top + barChartMargin.bottom)
@@ -54,6 +58,34 @@ function initBarChartToolTip() {
         .append('div')
         .attr('class', 'hover-tooltip')
         .attr("id", "bar-chart-tooltip");
+}
+
+function handleSort() {
+    const $sortButton = $("#sort-button");
+    $sortButton.append(sortUpHtml)
+    $sortButton.on("click", function() {
+        let newSort;
+        if($sortButton.find("svg").attr("id") == "sort-up-icon") {
+            newSort = sortDownHtml;
+        } else {
+            newSort = sortUpHtml;
+        }
+
+        switchSortIcon($sortButton, newSort);
+        selectedMunicipalities.sort((a,b) => {
+            if(newSort == sortDownHtml) {
+                return a.yearData[currentYear] - b.yearData[currentYear];
+            } else {
+                return b.yearData[currentYear] - a.yearData[currentYear];
+            }
+          }) 
+        updateBarCharts();
+    })
+}
+
+function switchSortIcon($sortButton, newSort) {
+    $sortButton.find("svg").remove();
+    $sortButton.append(newSort);
 }
 
 // Redraw entire bar charts when adding or removing selected municipalities
